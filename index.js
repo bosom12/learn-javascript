443ba46fc0029bc86dd9f972ae7671c94bcadb88
Daniel Adek <maildaniel.me1@gmail.com>
2019-05-01 18:27:25 +0100

    create subtract function

---
+++ index.js
@@ -0,0 +1,12 @@
+const subtract = (a, b) => {
+    if (typeof a !== 'number' || typeof b !== 'number'){
+        return `one of the argument is not a number`;
+    }
+    if (a < b ){
+        return 'a is less than b'
+    }
+ return a - b;
+}
+
+console.log(subtract(7, 12));
+
