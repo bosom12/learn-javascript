96359c5c4ea41e8a9f79618baf6db9568bf5f404
bosom12 <maildaniel.me1@gmail.com>
2019-05-02 23:34:00 +0100

    create addition and multiply algorithms

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const subtract = (a, b) => {
-    if (typeof a !== 'number' || typeof b !== 'number'){
+    if (typeof a !== 'number' || typeof b !== 'number') {
         return `one of the argument is not a number`;
     }
-    if (a < b ){
+    if (a < b) {
         return 'a is less than b'
     }
- return a - b;
+    return a - b;
 }
 
-console.log(subtract(12, 6));
+console.log(subtract(10, 6));
 
+const addition = (x, y) => {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return 'one of this is not a number';
+    }
+    
+    return x + y;
+}
+
+console.log(addition(12, 5))
+
+const multiply = (c, d) => {
+    if (typeof c !== 'number' || typeof d !== 'number') {
+        return 'one of this is not a number';
+    }
+   
+    return c * d;
+
+}
+
+console.log(multiply(4, 4))
